0f9977eda89c4fb1be0c3ae960fa9e1ba5a3bd70
dbhushan18 <57983786+dbhushan18@users.noreply.github.com>
2024-02-29 14:28:05 +0530

    Update authMiddleware.js

--- Middlewares/authMiddleware.js
+++ Middlewares/authMiddleware.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 
 const verifyToken = (req, res, next)=>{
     try{
-        const token = req.header("Authorization");
+        const token = req.header("token");
         if(!token){
             return res.status(401).json({message:"Unverified User"})
         }
